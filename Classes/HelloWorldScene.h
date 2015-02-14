0258be33d5f3d7ae23f7a0472d7ac539656a5987
loctho1995 <buidinhloctho1995@gmail.com>
2015-02-14 13:32:56 +0700

    work

---
+++ Classes/HelloWorldScene.h
@@ -0,0 +1,40 @@
+#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
+
+#include "cocos2d.h"
+namespace cocos2d
+{
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public interface class ICallback
+	{
+	public:
+		 virtual void CallFunc(Platform::String ^mess);
+	};
+
+	public ref class PlatformCenter sealed
+	{
+	public:
+		PlatformCenter();
+		static void setCallback(ICallback ^callback);
+		static void callFunc(Platform::String^ s);
+	};	
+};
+
+class HelloWorld : public cocos2d::Layer
+{
+public:
+    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    static cocos2d::Scene* createScene();
+
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+    virtual bool init();
+    
+    // a selector callback
+    void menuCloseCallback(cocos2d::Ref* pSender);
+    
+	void CallCSharp();
+    // implement the "static create()" method manually
+    CREATE_FUNC(HelloWorld);
+};
+
+#endif // __HELLOWORLD_SCENE_H__
